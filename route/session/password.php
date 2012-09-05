56f8cca4a073aebd1cfe2d715ba7271f35ac0127
Lazarus974 <ol@verman.fr>
2012-09-05 13:35:57 +0000

    Reworked password recovery
    git-svn-id: file:///home/svn/nat/session/branches/1_3@119 3d4b3179-e29e-49b3-9362-b375a6efb87c

--- route/session/password.php
+++ route/session/password.php
@@ -34,15 +34,11 @@ class wfr_session_session_password extends wf_route_request {
 	public function show() {
 		
 		/* init vars and get parameters */
-		$action = $this->wf->get_var("action");
-		$id = intval($this->wf->get_var("id"));
+		$recover = $this->wf->get_var("recovery");
 		$code = $this->wf->get_var("c");
-		$allowed = false;
 		$errors = array();
-		$firstname = '';
-		$lastname = '';
-		$email = '';
-		$this->user = null;
+		$allowed = false;
+		$user = null;
 		
 		/* process vars and sanatize */
 		if(isset($this->wf->ini_arr['session']['allow_pass_recovering']))
@@ -55,45 +51,22 @@ class wfr_session_session_password extends wf_route_request {
 		}
 		
 		/* do action */
-		if($action == "search") {
-			
-			$this->user = $this->search($errors);
-			
-			if(empty($errors)) {
-				// send an email ??
-				//$this->session_mail->mail_inscription($uid, $password);
-				
-				$code = $this->wf->generate_password(16);
-				$this->a_session->user->modify(
-					array("password_recovery" => $code),
-					$this->user['id']
-				);
-			}
-			
-			/* step back */
-			else
-				$action = "";
-		}
-		elseif($action == "recover") {
-			
-			/* get user back */
-			$user = $this->a_session->user->get("id", $id);
+		if($code != null) {
+			$user = $this->a_session->user->get("password_recovery", $code);
 			
 			if(isset($user[0])) {
-				$this->user = $user[0];
-				$is_hacking = !$this->recover($errors);
+				$password = $this->a_session->user->generate_password();
 				
-				if($is_hacking) {
-					$this->wf->display_error(403, $this->lang->ts("You are trying to change the password of someone else"));
-					exit(0);
-				}
-			}
-			else
-				$errors[] = $this->lang->ts("Sorry, we lose your identity, please try again");
-			
-			/* send mail and redirect to login */
-			if(empty($errors)) {
-				$this->session_mail->mail_password_changed($id, $this->wf->get_var("rec_password"));
+				$this->a_session->user->modify(
+					array(
+						"password" => $password,
+						"password_recovery" => "",
+					),
+					$user[0]['id']
+				);
+				
+				$this->session_mail->mail_password_recovered($user[0]["id"], $password);
+				
 				$this->wf->display_msg(
 					$this->lang->ts("Password changed !"),
 					$this->lang->ts("Your password was changed.<br/>We sent you an email with your new one.")
@@ -101,19 +74,34 @@ class wfr_session_session_password extends wf_route_request {
 				//$this->wf->redirector($this->wf->linker("/session/login"));
 				exit(0);
 			}
-			
-			/* step back */
-			else {
-				$action = "search";
-			}
+			else
+				$errors[] = $this->lang->ts("Something weird occurs, we did not find you. Do you mind giving it another try later ?");
 		}
-		
-		/* if user was found then */
-		if(!is_null($this->user)) {
-			$id = $this->user['id'];
-			$firstname = $this->user['firstname'];
-			$lastname = $this->user['name'];
-			$email = $this->user['email'];
+		else {
+			if(strlen($recover) > 0) {
+				$user = $this->a_session->user->get("username", $recover);
+				if(!isset($user[0])) {
+					$user = $this->a_session->user->get("email", $recover);
+					if(!isset($user[0]))
+						$errors[] = $this->lang->ts("User not found");
+				}
+			}
+			
+			if(empty($errors) && isset($user[0])) {
+				$code = $this->wf->generate_password(16);
+				$this->a_session->user->modify(
+					array("password_recovery" => $code),
+					$user[0]['id']
+				);
+				$this->session_mail->mail_password_recovery_request($user, $code);
+				
+				$this->wf->display_msg(
+					$this->lang->ts("Password request sent !"),
+					$this->lang->ts("We sent you an email with a link inside.<br/>Please go on this page and we will send you another password.")
+				);
+				$this->wf->redirector($this->wf->linker("/session/login"));
+				exit(0);
+			}
 		}
 		
 		/* admin settings */
@@ -121,13 +109,7 @@ class wfr_session_session_password extends wf_route_request {
 		$this->a_admin_html->set_backlink($this->wf->get_default_url());
 		
 		/* set tpl vars */
-		$this->tpl->set("last_action", $action);
 		$this->tpl->set("errors", $errors);
-		$this->tpl->set("id", $id);
-		$this->tpl->set("firstname", $firstname);
-		$this->tpl->set("lastname", $lastname);
-		$this->tpl->set("email", $email);
-		$this->tpl->set("code", $code);
 		
 		/* rendering */
 		$this->a_admin_html->rendering(
@@ -137,71 +119,5 @@ class wfr_session_session_password extends wf_route_request {
 		);
 		
 		exit(0);
-	}
-	
-	private function search(&$err) {
-		$u = null;
-		$recover = $this->wf->get_var("recovery");
-		
-		if(strlen($recover) > 0) {
-			$user = $this->a_session->user->get("username", $recover);
-			if(isset($user[0]))
-				$u = $user[0];
-			else {
-				$user = $this->a_session->user->get("email", $recover);
-				if(isset($user[0])) {
-					if($user[0]['remote_address'] == ip2long($_SERVER["REMOTE_ADDR"]))
-						$u = $user[0];
-					else
-						$err[] = $this->lang->ts("You can reset your password with an email address only from the last location where you logged in");
-				}
-				else
-					$err[] = $this->lang->ts("User not found");
-			}
-		}
-		else
-			$err[] = $this->lang->ts("You did not fill the form");
-		
-		return $u;
-	}
-	
-	private function recover(&$err) {
-		$id = (int) $this->wf->get_var("id");
-		$pass = $this->wf->get_var("rec_password");
-		$pass_confirm = $this->wf->get_var("rec_password_confirm");
-		$code = $this->wf->get_var("c");
-		
-		if(strlen($pass) > 0 && strlen($pass_confirm) > 0) {
-			if(strlen($pass) > 5) {
-				if($pass == $pass_confirm) {
-					if($id > 0 && !is_null($this->user)) {
-						if($code == $this->user['password_recovery']) {
-							
-							$this->a_session->user->modify(
-								array(
-									"password" => $pass,
-									"password_recovery" => "",
-								),
-								$id
-							);
-							
-							return true;
-						}
-						else
-							return false;
-					}
-					else
-						$err[] = $this->lang->ts("Something weird happen, we lost your identity");
-				}
-				else
-					$err[] = $this->lang->ts("Passwords does not match");
-			}
-			else
-				$err[] = $this->lang->ts("Your password is too short, use 6 characters at least");
-		}
-		else
-			$err[] = $this->lang->ts("You did not fill the form");
-		
-		return true;
 	}
 }
--- var/tpl/session/password_recovery.tpl
+++ var/tpl/session/password_recovery.tpl
@@ -5,14 +5,9 @@
 	</div>
 	
 	<p class="intro">
-		%{if($last_action == "search")}%
-			%{@ "Fine %s %s, type your new password and we will reset it.", $firstname, $lastname}%<br/>
-			%{@ "We will send you an email to \"%s\" with your new password, please keep it in your mailbox to remember your password this time !", $email}%<br/>
-		%{else}%
-			%{@ "Did you lose your password ? Then this page is for you."}%<br/>
-			%{@ "You can type your username or your email address to recover your password."}%<br/>
-			%{@ "We will send you an email with a link and you will be able to choose another one."}%
-		%{/if}%
+		%{@ "Did you lose your password ? Then this page is for you."}%<br/>
+		%{@ "You can type your username or your email address to recover your password."}%<br/>
+		%{@ "We will send you an email with a link to a page where you will be able to choose another one."}%
 		<br/><br/>
 		%{@ "If you find your password back, "}%<a href="%{link '/session/login'}%">%{@ "please log in here"}%</a>.
 	</p>
@@ -30,28 +25,6 @@
 	%{/if}%
 	
 	<form action="?" method="post">
-		%{if($last_action == "search")}%
-		<input type="hidden" name="id" value="%{$id}%" />
-		<input type="hidden" name="c" value="%{$code}%" />
-		<input type="hidden" name="action" value="recover" />
-		<ul data-role="listview" data-inset="true">
-			
-			<li data-role="fieldcontain">
-				<label for="rec_password">%{@ 'New password :'}%</label>
-				<input type="password" name="rec_password" id="rec_password" data-mini="true" />
-			</li>
-			
-			<li data-role="fieldcontain">
-				<label for="rec_password_confirm">%{@ 'Confirm :'}%</label>
-				<input type="password" name="rec_password_confirm" id="rec_password_confirm" data-mini="true" />
-			</li>
-			
-			<li data-role="fieldcontain">
-				<button type="submit" data-theme="b">%{@ 'Recover'}%</button>
-			</li>
-		</ul>
-		%{else}%
-		<input type="hidden" name="action" value="search" />
 		<ul data-role="listview" data-inset="true">
 			
 			<li data-role="fieldcontain">
@@ -63,6 +36,5 @@
 				<button type="submit" data-theme="b">%{@ 'Search'}%</button>
 			</li>
 		</ul>
-		%{/if}%
 	</form>
 </div>
